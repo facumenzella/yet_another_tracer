9473d99e2e517eb8e24d63a81aef239d2cd2693b
Facundo Menzella <fmenzella@monits.com>
2015-03-11 09:37:49 -0300

    length precalculated in Vector3d

--- src/main/java/ar/edu/itba/it/cg/yart/geometry/Vector3.java
+++ src/main/java/ar/edu/itba/it/cg/yart/geometry/Vector3.java
@@ -5,11 +5,13 @@ public class Vector3 {
 	public double x;
 	public double y;
 	public double z;
+	public double length;
 	
 	public Vector3(double x, double y, double z) {
 		this.x = x;
 		this.y = y;
 		this.z = z;
+		this.length = this.length();
 	}
 	
 	public static Vector3 xAxis() {
