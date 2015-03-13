4dc37c9ac2fbfd288050e7e82c05450e193e21cd
Facundo Menzella <fmenzella@monits.com>
2015-03-13 10:14:59 -0300

    comments added to up and lookat vector

--- src/main/java/ar/edu/itba/it/cg/yart/raytracer/world/World.java
+++ src/main/java/ar/edu/itba/it/cg/yart/raytracer/world/World.java
@@ -57,22 +57,22 @@ public class World {
 	public static World spheresWorld(final ViewPlane vp) {
 		final Tracer tracer = new Tracer();
 		final Point3 eye = new Point3(0,0,30);
-		final Point3 lookat = new Point3(0,0,1);
-		final Vector3d up = new Vector3d(0,1,0);
+		final Point3 lookat = new Point3(-30,0,-30); // point where we look at
+		final Vector3d up = new Vector3d(0,1,0); // up vector, rotates around the camera z-axis
 		final double distance = 40;
 		final World world = new World(vp, new PinholeCamera(tracer, eye, lookat, up, distance), tracer);
 		final Sphere s1 = new Sphere(new Point3(-30,0.0f,-30), 30.0f);
 		s1.color = Color.redColor();
 		final Sphere s2 = new Sphere(new Point3(30,0,-30), 30.0f);
 		s2.color = Color.blueColor();
-		final Sphere s3 = new Sphere(new Point3(0,-50,-30), 40.0f);
-		s3.color = Color.greenColor();
+//		final Sphere s3 = new Sphere(new Point3(0,-50,-30), 40.0f);
+//		s3.color = Color.greenColor();
 //		final Sphere s4 = new Sphere(new Point3(-20,0,-100), 5.0f);
 //		s4.color = Color.yellowColor();
 		
 		world.addObject(s1);
 		world.addObject(s2);
-		world.addObject(s3);
+//		world.addObject(s3);
 //		world.addObject(s4);
 		
 		return world;
