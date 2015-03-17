56131fae3bebb214d9357a6352e69d7c8e2ff418
Joe <jose.noriega.defferrari@gmail.com>
2015-03-16 21:20:54 -0300

    changed floats to doubles

--- src/main/java/ar/edu/itba/it/cg/yart/light/Matte.java
+++ src/main/java/ar/edu/itba/it/cg/yart/light/Matte.java
@@ -1,6 +1,10 @@
 package ar.edu.itba.it.cg.yart.light;
 
+
+import java.util.List;
+
 import ar.edu.itba.it.cg.yart.color.Color;
+import ar.edu.itba.it.cg.yart.geometry.Vector3d;
 import ar.edu.itba.it.cg.yart.light.brdf.Lambertian;
 import ar.edu.itba.it.cg.yart.raytracer.ShadeRec;
 
@@ -12,8 +16,34 @@ public class Matte extends Material {
 
 	@Override
 	public Color shade(ShadeRec sr) {
-		//TODO: PAGE 271
-		return null;
+		final Vector3d wo = sr.ray.direction.inverse();
+		final Color colorL = ambientBRDF.rho(sr, wo).multiply(sr.world.ambientLight.L(sr));
+		final List<Light> lights = sr.world.lights;
+		
+		for(final Light light : lights) {
+			final Vector3d wi = light.getDirection(sr);
+			double ndotwi = sr.normal.dot(wi);
+			
+			if (ndotwi > 0.0) {
+				final Color aux = diffuseBRDF.f(sr, wo, wi).multiply(light.L(sr)).multiply(ndotwi);
+				colorL.addEquals(aux);
+			}
+		}
+		
+		return colorL;
+	}
+	
+	public void setKa(final double ka) {
+		ambientBRDF.setKd(ka);
+	}
+	
+	public void setKd(final double kd) {
+		diffuseBRDF.setKd(kd);
+	}
+	
+	public void setCd(final Color cd) {
+		ambientBRDF.setCd(cd);
+		diffuseBRDF.setCd(cd);
 	}
 	
 }
