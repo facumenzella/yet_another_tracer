213571df88bd577291d46bc0b0492fec706f615a
Gustavo Del Giudice <gdelgiud@itba.edu.ar>
2015-03-18 17:33:55 -0300

    Brought back the UI renderer.

--- src/main/java/ar/edu/itba/it/cg/yart/raytracer/camera/CameraAbstract.java
+++ src/main/java/ar/edu/itba/it/cg/yart/raytracer/camera/CameraAbstract.java
@@ -2,8 +2,6 @@ package ar.edu.itba.it.cg.yart.raytracer.camera;
 
 import ar.edu.itba.it.cg.yart.geometry.Point3;
 import ar.edu.itba.it.cg.yart.geometry.Vector3d;
-import ar.edu.itba.it.cg.yart.matrix.ArrayIntegerMatrix;
-import ar.edu.itba.it.cg.yart.raytracer.Bucket;
 import ar.edu.itba.it.cg.yart.raytracer.Tracer;
 
 public abstract class CameraAbstract implements Camera {
@@ -13,17 +11,7 @@ public abstract class CameraAbstract implements Camera {
 	protected Vector3d up;
 	protected Vector3d u, v, w;
 	protected Tracer tracer;
-	protected Callbacks callbacks;
 	float exposue_time;
-	
-	public interface Callbacks {
-		public void onBucketFinished(ArrayIntegerMatrix result, Bucket bucket);
-		public void onRenderFinished(ArrayIntegerMatrix result);
-	}
-	
-	public void setCallbacks(final Callbacks callbacks) {
-		this.callbacks = callbacks;
-	}
 
 	protected CameraAbstract(final Tracer tracer, final Point3 eye, 
 			final Point3 lookat, final Vector3d up) {
