449440dfb26cf015fdf30d9069d7ae0d05f8278a
Facundo Menzella <fmenzella@monits.com>
2015-03-26 16:22:53 -0300

    yart config provider working

---
+++ src/main/java/ar/edu/itba/it/cg/yart/utils/config/YartConfigReader.java
@@ -0,0 +1,33 @@
+package ar.edu.itba.it.cg.yart.utils.config;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+public class YartConfigReader {
+
+	private final Properties properties;
+	private static final String BASE = "src/main/resources";
+	private static final String PROPERTIES_FILE = "config.properties";
+
+	protected YartConfigReader() {
+		this.properties = new Properties();
+		this.load();
+	}
+
+	private void load() {
+		StringBuilder builder = new StringBuilder();
+		builder.append(BASE).append(PROPERTIES_FILE);
+		try {
+			this.properties.load(new FileInputStream(builder.toString()));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	protected String getKey(final String key) {
+		final String aKey = this.properties.getProperty(key);
+		return aKey;
+	}
+
+}
