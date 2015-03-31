1e2c403883752d517a4420d60b82453ffb3e65ed
Facundo Menzella <fmenzella@monits.com>
2015-03-31 19:42:02 -0300

    body removed was not useful

--- src/main/java/ar/edu/itba/it/cg/yart/geometry/Body.java
+++
@@ -1,8 +0,0 @@
-package ar.edu.itba.it.cg.yart.geometry;
-
-import ar.edu.itba.it.cg.yart.acceleration_estructures.Side;
-import ar.edu.itba.it.cg.yart.geometry.primitives.BoundingBox;
-
-public interface Body {
-	public Side insideOf(final BoundingBox box);
-}
