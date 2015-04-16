ae1ecc89f8251c14baf8dc465944d2d140b2a281
Gustavo Del Giudice <gdelgiud@itba.edu.ar>
2015-04-15 21:09:03 -0300

    Added more classes used by the parser.

--- src/main/java/ar/edu/itba/it/cg/yart/parser/Attribute.java
+++ src/main/java/ar/edu/itba/it/cg/yart/parser/Attribute.java
@@ -1,5 +1,35 @@
 package ar.edu.itba.it.cg.yart.parser;
 
-public class Attribute {
+import java.util.ArrayList;
+import java.util.List;
 
+public class Attribute {
+	
+	public enum AttributeType {
+		WORLD,
+		ATTRIBUTE,
+		OBJECT,
+		TRANSFORM
+	}
+
+	private final AttributeType type;
+	private List<Identifier> identifiers;
+	
+	public Attribute(final AttributeType type) {
+		this.type = type;
+	}
+	
+	public void addIdentifier(final Identifier identifier) {
+		if (identifiers == null) {
+			identifiers = new ArrayList<Identifier>();
+		}
+		
+		if (identifier != null) {
+			identifiers.add(identifier);
+		}		
+	}
+	
+	public AttributeType getType() {
+		return type;
+	}
 }
