b93faba0da95dd2df353d84ae3339df586a1967a
Gustavo Del Giudice <gdelgiud@alu.itba.edu.ar>
2015-04-17 20:48:14 -0300

    The Parser now reads all elements into some kind of tree structure.

--- src/main/java/ar/edu/itba/it/cg/yart/parser/Identifier.java
+++ src/main/java/ar/edu/itba/it/cg/yart/parser/Identifier.java
@@ -1,10 +1,74 @@
 package ar.edu.itba.it.cg.yart.parser;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Identifier {
 
 	public enum IdentifierType {
-		LOOKAT,
-		CAMERA,
-		FILM
+		LOOKAT("LookAt"),
+		CAMERA("Camera"),
+		FILM("Film"),
+		SHAPE("Shape");
+		
+		private String name;
+		
+		private IdentifierType(final String name) {
+			this.name = name;
+		}
+		
+		public String getName() {
+			return name;
+		}
+	}
+
+	private List<Property> properties;
+	private Object parameter;
+	private IdentifierType type;
+	
+	public static IdentifierType getByName(final String name) {
+		for (IdentifierType type : IdentifierType.values()) {
+			if (type.getName().equals(name)) {
+				return type;
+			}
+		}
+		
+		return null;
+	}
+
+	public Identifier(final IdentifierType type, final String[] args) {
+		this.type = type;
+	}
+
+	public void addProperty(final Property property) {
+		if (properties == null) {
+			properties = new ArrayList<Property>();
+		}
+
+		if (property != null) {
+			properties.add(property);
+		}
+	}
+	
+	public IdentifierType getType() {
+		return type;
+	}
+	
+	public Object getParamter() {
+		return parameter;
+	}
+	
+	public List<Property> getProperties() {
+		return properties;
+	}
+	
+	@Override
+	public String toString() {
+		String children = "Empty";
+		if (properties != null && !properties.isEmpty()) {
+			children = properties.size() + " children";
+		}
+		
+		return type.toString() + "(" + children + ")";
 	}
 }
