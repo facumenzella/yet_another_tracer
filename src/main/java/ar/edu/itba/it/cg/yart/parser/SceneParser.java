c3bb8d0df4fe85d52c8389b99ea6e5def6df3f43
Gustavo Del Giudice <gdelgiud@alu.itba.edu.ar>
2015-04-17 20:48:14 -0300

    The Parser now reads all elements into some kind of tree structure.

--- src/main/java/ar/edu/itba/it/cg/yart/parser/SceneParser.java
+++ src/main/java/ar/edu/itba/it/cg/yart/parser/SceneParser.java
@@ -6,17 +6,13 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.text.ParseException;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.Scanner;
 
 import org.apache.commons.lang3.StringUtils;
 
-import ar.edu.itba.it.cg.yart.geometry.Point3;
-import ar.edu.itba.it.cg.yart.geometry.Vector3d;
-import ar.edu.itba.it.cg.yart.light.materials.Material;
 import ar.edu.itba.it.cg.yart.parser.Attribute.AttributeType;
+import ar.edu.itba.it.cg.yart.parser.Identifier.IdentifierType;
 
 public class SceneParser {
 	
@@ -26,59 +22,50 @@ public class SceneParser {
 		ATTRIBUTE,
 		END
 	}
-	
-	private enum Identifier {
-		LOOKAT,
-		CAMERA,
-		FILM,
-	}
 
 	private final String filePath;
-	private Map<String, Material> materials;
 	
 	private ParserStatus status;
-	private Identifier identifier;
+	
 	private Attribute currentAttribute;
-	private List<Property> accProperties;
+	private Identifier currentIdentifier;
+	
+	private List<Attribute> attributes = new ArrayList<Attribute>();
+	private List<Identifier> globalIdentifiers = new ArrayList<Identifier>();
+	private List<Identifier> accIdentifiers = new ArrayList<Identifier>();
+	private List<Property> accProperties = new ArrayList<Property>();
 	
 	public SceneParser(final String filePath) {
 		this.filePath = filePath;
-		materials = new HashMap<String, Material>();
 		status = ParserStatus.GLOBAL;
-		accProperties = new ArrayList<Property>();
 	}
 	
 	public void parseFile() throws IOException, ParseException {
 		Path path = Paths.get(filePath);
 		Scanner scanner =  new Scanner(path, StandardCharsets.UTF_8.name());
 		while (scanner.hasNextLine() && status != ParserStatus.END) {
-			processLine(scanner.nextLine());
+			String rawLine = scanner.nextLine().trim();
+			String uncommentedLine = StringUtils.substringBefore(rawLine, "#");
+			
+			if (uncommentedLine != null && !uncommentedLine.isEmpty()) {
+				processLine(uncommentedLine);
+			}
 		}
 		applyProperties();
 		scanner.close();
 	}
 	
 	public void processLine(final String line) throws ParseException {
-		Scanner scanner = new Scanner(line);
+		String first = StringUtils.substringBefore(line, " ");
+		System.out.println(first);
 		
-		if (!scanner.hasNext()) {
-			scanner.close();
-			return;
-		}
-		
-		String first = scanner.next();
-		if (first.startsWith("#")) { // Is a comment
-			
-		}
-		else if (first.startsWith("\"")) { // Is a property
+		if (first.charAt(0) == '"') { // Is a property
 			processProperties(line);
 		}
-		else if (first.matches("^[A-Z].*")) { // Is an attribute
+		else if (Character.isUpperCase(first.charAt(0))) { // Is an attribute
 			applyProperties();
-			processAttribute(first, line);
+			processAttribute(first, StringUtils.substringAfter(line, " ").split("\\s"));
 		}
-		
-		scanner.close();
 	}
 	
 	public void processProperties(final String line) throws ParseException {
@@ -95,65 +82,102 @@ public class SceneParser {
 		}
 	}
 	
-	public void processAttribute(final String attribute, final String line) throws ParseException {
-		String[] args = line.split("\\s");
-		
-		switch (status) {
-		case ATTRIBUTE:
-			break;
-		case GLOBAL:
-			if (attribute.equals("WorldBegin")) {
-				status = ParserStatus.WORLD;
+	public void processAttribute(final String attribute, final String[] args) throws ParseException {
+		// First, check if we're dealing with an Identifier
+		IdentifierType identifierType = Identifier.getByName(attribute);
+		if (identifierType != null) {
+			currentIdentifier = new Identifier(identifierType, args);
+			accIdentifiers.add(currentIdentifier);
+		}
+		// Maybe it's an Attribute
+		else if (attribute.equals("AttributeBegin")) {
+			if (currentAttribute != null) {
+				throw new ParseException("Syntax error: Cannot create an attribute inside another", 0);
 			}
-			else if (attribute.equals("LookAt")) {
-				if (args.length != 10) {
-					throw new ParseException("Syntax error: LookAt expects 9 doubles, found " + (args.length - 1), 0);
-				}
-				
-				// TODO Use these?
-				Point3 eye = new Point3(Double.valueOf(args[1]), Double.valueOf(args[2]), Double.valueOf(args[3]));
-				Point3 lookat = new Point3(Double.valueOf(args[4]), Double.valueOf(args[5]), Double.valueOf(args[6]));
-				Vector3d up = new Vector3d(Double.valueOf(args[7]), Double.valueOf(args[8]), Double.valueOf(args[9]));
-				identifier = Identifier.LOOKAT;
+			
+			currentAttribute = new Attribute(AttributeType.ATTRIBUTE, args);
+			attributes.add(currentAttribute);
+			status = ParserStatus.ATTRIBUTE;
+		}
+		else if (attribute.equals("AttributeEnd")) {
+			if (currentAttribute == null || currentAttribute.getType() != AttributeType.ATTRIBUTE) {
+				throw new ParseException("Syntax error: AttributeEnd found without matching AttributeBegin", 0);
 			}
-			else if (attribute.equals("Camera")) {
-				String type = StringUtils.substringBetween(line, "\"", "\"");
-				if (type.equals("perspective")) {
-					// TODO Instance camera
-					System.out.println("Perspective camera alright");
-				}
-				identifier = Identifier.CAMERA;
+			
+			closeAttribute();
+			status = ParserStatus.WORLD;
+		}
+		else if (attribute.equals("ObjectBegin")) {
+			if (currentAttribute != null) {
+				throw new ParseException("Syntax error: Cannot create an attribute inside another", 0);
 			}
-			else if (attribute.equals("Film")) {
-				identifier = Identifier.FILM;
+			
+			currentAttribute = new Attribute(AttributeType.OBJECT, args);
+			attributes.add(currentAttribute);
+			status = ParserStatus.ATTRIBUTE;
+		}
+		else if (attribute.equals("ObjectEnd")) {
+			if (currentAttribute == null || currentAttribute.getType() != AttributeType.OBJECT) {
+				throw new ParseException("Syntax error: ObjectEnd found without matching ObjectBegin", 0);
 			}
-			break;
-		case WORLD:
-			if (attribute.equals("AttributeBegin")) {
-				currentAttribute = new Attribute(AttributeType.ATTRIBUTE);
-				status = ParserStatus.ATTRIBUTE;
+			
+			closeAttribute();
+			status = ParserStatus.WORLD;
+		}
+		else if (attribute.equals("WorldBegin")) {
+			if (status != ParserStatus.GLOBAL) {
+				throw new ParseException("Syntax error: World must be defined with global scope", 0);
 			}
-			else if (attribute.equals("AttributeEnd")) {
-				status = ParserStatus.WORLD;
+			
+			// Commit all global identifiers
+			closeAttribute();
+			status = ParserStatus.WORLD;
+		}
+		else if (attribute.equals("WorldEnd")) {
+			if (status == ParserStatus.ATTRIBUTE) {
+				throw new ParseException("Syntax error: WorldEnd found while an Attribute is still open", 0);
 			}
-			else if (attribute.equals("ObjectBegin")) {
-				currentAttribute = new Attribute(AttributeType.OBJECT);
-				status = ParserStatus.ATTRIBUTE;
+			else if (status != ParserStatus.WORLD) {
+				throw new ParseException("Syntax error: WorldEnd found without matching WorldBegin", 0);
 			}
-			else if (attribute.equals("WorldEnd")) {
-				status = ParserStatus.END;
-				applyProperties();
+			status = ParserStatus.END;
+		}
+	}
+	
+	public void closeAttribute() {
+		applyProperties();
+		applyIdentifiers();
+		currentAttribute = null;
+	}
+	
+	public void applyIdentifiers() {
+		if (status == ParserStatus.GLOBAL) {
+			for (Identifier i : accIdentifiers) {
+				globalIdentifiers.add(i);
 			}
-			break;
+		}
+		else if (currentAttribute != null) {
+			for (Identifier i : accIdentifiers) {
+				currentAttribute.addIdentifier(i);
+			}
+		}
+		else {
+			// TODO Warning here, we're trying to add identifiers without any Attribute
 		}
 		
-		//System.out.println("Attribute: " + attribute + "  Param: " + args[1]);
+		accIdentifiers.clear();
 	}
 	
 	public void applyProperties() {
-		for (Property p : accProperties) {
-			System.out.println(p.getName() + "-->" + p.getValue());
+		if (currentIdentifier != null) {
+			for (Property p : accProperties) {
+				currentIdentifier.addProperty(p);
+			}
 		}
+		else {
+			// TODO Warning here, we're trying to add properties without any Identifier
+		}
+		
 		accProperties.clear();
 	}
 }
