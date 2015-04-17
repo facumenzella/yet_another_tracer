29e52004d96e341d31e96c78fd8a645ab063a9da
Gustavo Del Giudice <gdelgiud@alu.itba.edu.ar>
2015-04-17 20:48:14 -0300

    The Parser now reads all elements into some kind of tree structure.

--- src/main/java/ar/edu/itba/it/cg/yart/parser/Attribute.java
+++ src/main/java/ar/edu/itba/it/cg/yart/parser/Attribute.java
@@ -1,7 +1,10 @@
 package ar.edu.itba.it.cg.yart.parser;
 
+import java.text.ParseException;
 import java.util.ArrayList;
 import java.util.List;
+
+import org.apache.commons.lang3.StringUtils;
 
 public class Attribute {
 	
@@ -13,10 +16,25 @@ public class Attribute {
 	}
 
 	private final AttributeType type;
+	private Object parameter;
 	private List<Identifier> identifiers;
 	
-	public Attribute(final AttributeType type) {
+	public Attribute(final AttributeType type, final String[] args) throws ParseException {
 		this.type = type;
+		
+		if (type == AttributeType.OBJECT) {
+			if (args == null || args.length == 0 || args[0].isEmpty()) {
+				throw new ParseException("Syntax error: ObjectBegin expects at least one parameter", 0);
+			}
+			
+			String strParam = StringUtils.substringBetween(args[0], "\"");
+			
+			if (strParam == null || strParam.isEmpty()) {
+				throw new ParseException("Syntax error: ObjectBegin expects at least one parameter", 0);
+			}
+			
+			parameter = strParam;
+		}
 	}
 	
 	public void addIdentifier(final Identifier identifier) {
@@ -29,7 +47,20 @@ public class Attribute {
 		}		
 	}
 	
+	public List<Identifier> getIdentifiers() {
+		return identifiers;
+	}
+	
 	public AttributeType getType() {
 		return type;
+	}
+	
+	public Object getParamter() {
+		return parameter;
+	}
+	
+	@Override
+	public String toString() {
+		return type.toString();
 	}
 }
