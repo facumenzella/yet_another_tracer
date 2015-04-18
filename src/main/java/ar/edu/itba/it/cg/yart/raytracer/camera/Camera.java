14699fbd87068328a5da6e8fd659ef55e8bb6b38
Gustavo Del Giudice <gdelgiud@alu.itba.edu.ar>
2015-04-18 20:35:37 -0300

    Reworked various methods so that they only take the RayTracer as parameter.

--- src/main/java/ar/edu/itba/it/cg/yart/raytracer/camera/Camera.java
+++ src/main/java/ar/edu/itba/it/cg/yart/raytracer/camera/Camera.java
@@ -5,14 +5,12 @@ import ar.edu.itba.it.cg.yart.geometry.Vector3d;
 import ar.edu.itba.it.cg.yart.matrix.ArrayIntegerMatrix;
 import ar.edu.itba.it.cg.yart.raytracer.ViewPlane;
 import ar.edu.itba.it.cg.yart.raytracer.buckets.Bucket;
-import ar.edu.itba.it.cg.yart.raytracer.tracer.Tracer;
-import ar.edu.itba.it.cg.yart.raytracer.world.World;
+import ar.edu.itba.it.cg.yart.raytracer.interfaces.RayTracer;
 
 public interface Camera {
 	
 	public ViewPlane getViewPlane();
-	public void renderScene(final Bucket bucket, final World world,
-			final ArrayIntegerMatrix result, final Tracer tracer, final int numSamples);
+	public void renderScene(final Bucket bucket, final RayTracer rayTracer, final ArrayIntegerMatrix result);
 	
 	public void setViewParameters(final Point3 eye, final Point3 lookAt, final Vector3d up);
 }
