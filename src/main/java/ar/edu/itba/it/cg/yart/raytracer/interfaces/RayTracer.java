eb5a4eabae8af41beb0ddc84a566f1fc9ad4646b
Gustavo Del Giudice <gdelgiud@alu.itba.edu.ar>
2015-04-18 16:17:37 -0300

    Removed Camera from World. It's now managed by the RayTracer.

--- src/main/java/ar/edu/itba/it/cg/yart/raytracer/interfaces/RayTracer.java
+++ src/main/java/ar/edu/itba/it/cg/yart/raytracer/interfaces/RayTracer.java
@@ -2,6 +2,7 @@ package ar.edu.itba.it.cg.yart.raytracer.interfaces;
 
 import ar.edu.itba.it.cg.yart.matrix.ArrayIntegerMatrix;
 import ar.edu.itba.it.cg.yart.raytracer.SimpleRayTracer.RaytracerCallbacks;
+import ar.edu.itba.it.cg.yart.raytracer.camera.Camera;
 import ar.edu.itba.it.cg.yart.raytracer.world.World;
 
 public interface RayTracer {
@@ -13,4 +14,7 @@ public interface RayTracer {
 	public int getVerticalRes();
 	public int getBucketSize();
 	public void setCallbacks(final RaytracerCallbacks callbacks);
+	
+	public void setCamera(final Camera camera);
+	public Camera getCamera();
 }
