f0e7ac2b7ec67d1eafa14eab6e86b990aaf069cf
Gustavo Del Giudice <gdelgiud@itba.edu.ar>
2015-04-22 20:08:21 -0300

    Recalculate the ViewPlane after its parameters change.

--- src/main/java/ar/edu/itba/it/cg/yart/raytracer/interfaces/RayTracer.java
+++ src/main/java/ar/edu/itba/it/cg/yart/raytracer/interfaces/RayTracer.java
@@ -4,6 +4,7 @@ import ar.edu.itba.it.cg.yart.geometry.Point3;
 import ar.edu.itba.it.cg.yart.geometry.Vector3d;
 import ar.edu.itba.it.cg.yart.matrix.ArrayIntegerMatrix;
 import ar.edu.itba.it.cg.yart.raytracer.SimpleRayTracer.RaytracerCallbacks;
+import ar.edu.itba.it.cg.yart.raytracer.ViewPlane;
 import ar.edu.itba.it.cg.yart.raytracer.camera.Camera;
 import ar.edu.itba.it.cg.yart.raytracer.tracer.Tracer;
 import ar.edu.itba.it.cg.yart.raytracer.world.World;
@@ -19,6 +20,7 @@ public interface RayTracer {
 	public int getBucketSize();
 	public void setCallbacks(final RaytracerCallbacks callbacks);
 	
+	public ViewPlane getViewPlane();
 	public Tracer getTracer();
 	public int getNumSamples();
 	public void setNumSamples(final int numSamples);
