d442eae36a4a54a70352596721694433b31e9464
Facundo Menzella <fmenzella@monits.com>
2015-04-26 20:27:39 -0300

    directional lights uses tree

--- src/main/java/ar/edu/itba/it/cg/yart/light/Directional.java
+++ src/main/java/ar/edu/itba/it/cg/yart/light/Directional.java
@@ -1,11 +1,11 @@
 package ar.edu.itba.it.cg.yart.light;
 
 import ar.edu.itba.it.cg.yart.color.Color;
-import ar.edu.itba.it.cg.yart.geometry.Point3;
 import ar.edu.itba.it.cg.yart.geometry.Vector3d;
-import ar.edu.itba.it.cg.yart.geometry.primitives.GeometricObject;
 import ar.edu.itba.it.cg.yart.raytracer.Ray;
 import ar.edu.itba.it.cg.yart.raytracer.ShadeRec;
+import ar.edu.itba.it.cg.yart.raytracer.tracer.HitTracer;
+import ar.edu.itba.it.cg.yart.raytracer.tracer.SimpleHitTracer;
 
 public class Directional extends LightAbstract{
 	
@@ -13,6 +13,7 @@ public class Directional extends LightAbstract{
 	private final Color color;
 	private final Color shadeColor;
 	private final Vector3d dir;
+	private final HitTracer tracer;
 	
 	public Directional(final double ls, final Color color, final Vector3d direction) {
 		super();
@@ -20,6 +21,7 @@ public class Directional extends LightAbstract{
 		this.color = color;
 		this.shadeColor = color.multiply(ls);
 		this.dir = direction.normalizedVector();
+		this.tracer = new SimpleHitTracer();
 	}
 	@Override
 	public Vector3d getDirection(ShadeRec sr) {
@@ -33,11 +35,9 @@ public class Directional extends LightAbstract{
 	@Override
 	public boolean inShadow(Ray ray, ShadeRec sr) {
 		double t;		
-		for(final GeometricObject object : sr.world.getObjects()) {
-			t = object.shadowHit(ray);
-			if(t != Double.NEGATIVE_INFINITY) {
+		t = sr.world.getTree().traceRayHit(ray, tracer);
+		if(t != Double.NEGATIVE_INFINITY) {
 				return true;
-			}
 		}
 		return false;
 	}
