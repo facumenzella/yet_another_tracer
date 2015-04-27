b974e7f2de5c6c83e39e886d703701640ceadd81
Facundo Menzella <fmenzella@monits.com>
2015-04-26 23:56:11 -0300

    clasified lights in world for better processing in materials

--- src/main/java/ar/edu/itba/it/cg/yart/light/materials/Phong.java
+++ src/main/java/ar/edu/itba/it/cg/yart/light/materials/Phong.java
@@ -10,62 +10,77 @@ import ar.edu.itba.it.cg.yart.light.brdf.Lambertian;
 import ar.edu.itba.it.cg.yart.raytracer.Ray;
 import ar.edu.itba.it.cg.yart.raytracer.ShadeRec;
 
-public class Phong extends MaterialAbstract{
-	
+public class Phong extends MaterialAbstract {
+
 	private final Lambertian ambientBRDF = new Lambertian();
 	private final Lambertian diffuseBRDF = new Lambertian();
 	private final GlossySpecular specularBRDF = new GlossySpecular();
-	
+
 	@Override
 	public Color shade(ShadeRec sr) {
-		final Vector3d wo = sr.ray.direction.inverse();
+		Vector3d wo = null; // we compute it if its really necesary
 		final Color colorL = ambientBRDF.rho(sr, wo);
 		colorL.multiplyEquals(sr.world.getAmbientLight().L(sr));
-		
-		final List<Light> lights = sr.world.getLights();
-		
-		for(final Light light : lights) {
+
+		final List<Light> castShadowLights = sr.world.getCastShadowLights();
+		final List<Light> doNotCastShadowLights = sr.world
+				.getDoNotCastShadowLights();
+
+		for (Light light : doNotCastShadowLights) {
 			final Vector3d wi = light.getDirection(sr);
 			double ndotwi = sr.normal.dot(wi);
-			
+
+			if (ndotwi > 0.0) {
+				wo = sr.ray.direction.inverse();
+				Color aux = diffuseBRDF.f(sr, wo, wi);
+				aux.addEquals(specularBRDF.f(sr, wo, wi));
+				aux.multiplyEquals(light.L(sr));
+				aux.multiplyEquals(ndotwi);
+				colorL.addEquals(aux);
+			}
+		}
+
+		for (final Light light : castShadowLights) {
+			final Vector3d wi = light.getDirection(sr);
+			double ndotwi = sr.normal.dot(wi);
+
 			if (ndotwi > 0.0) {
 				boolean inShadow = false;
-				
-				if(light.castShadows()){
-					Ray shadowRay = new Ray(sr.hitPoint,wi);
-					inShadow = light.inShadow(shadowRay, sr);
-				}
-				if(!inShadow){
+
+				Ray shadowRay = new Ray(sr.hitPoint, wi);
+				inShadow = light.inShadow(shadowRay, sr);
+				if (!inShadow) {
+					wo = sr.ray.direction.inverse();
 					Color aux = diffuseBRDF.f(sr, wo, wi);
 					aux.addEquals(specularBRDF.f(sr, wo, wi));
 					aux.multiplyEquals(light.L(sr));
 					aux.multiplyEquals(ndotwi);
-					colorL.addEquals(aux);					
+					colorL.addEquals(aux);
 				}
 			}
 		}
-		
+
 		return colorL;
 	}
-	
+
 	public void setKa(final double ka) {
 		ambientBRDF.setKd(ka);
 	}
-	
+
 	public void setKd(final double kd) {
 		diffuseBRDF.setKd(kd);
 	}
-	
+
 	public void setKs(final double ks) {
 		specularBRDF.setKs(ks);
 	}
-	
+
 	public void setCd(final Color cd) {
 		ambientBRDF.setCd(cd);
 		diffuseBRDF.setCd(cd);
 		specularBRDF.setCs(cd);
 	}
-	
+
 	public void setExp(final double exp) {
 		specularBRDF.setExp(exp);
 	}
