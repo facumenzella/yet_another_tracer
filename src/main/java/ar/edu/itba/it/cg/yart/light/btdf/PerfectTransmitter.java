652c6e02547df47e582777634d28566b908c3670
Facundo Menzella <fmenzella@monits.com>
2015-04-26 22:01:37 -0300

    efficient perfect transmitter

--- src/main/java/ar/edu/itba/it/cg/yart/light/btdf/PerfectTransmitter.java
+++ src/main/java/ar/edu/itba/it/cg/yart/light/btdf/PerfectTransmitter.java
@@ -1,6 +1,7 @@
 package ar.edu.itba.it.cg.yart.light.btdf;
 
 import ar.edu.itba.it.cg.yart.color.Color;
+import ar.edu.itba.it.cg.yart.geometry.MutableVector3d;
 import ar.edu.itba.it.cg.yart.geometry.Vector3d;
 import ar.edu.itba.it.cg.yart.raytracer.ShadeRec;
 
@@ -31,23 +32,30 @@ public class PerfectTransmitter extends BTDF {
 
 	@Override
 	public Color sample_f(ShadeRec sr, Vector3d wo, Vector3d wt) {
-		Vector3d n = sr.normal;
+		MutableVector3d n = new MutableVector3d(sr.normal);
+		MutableVector3d mWo = new MutableVector3d(wo);
+
 		double cosThetai = n.dot(wo);
 		double eta = ior;
 
 		if (cosThetai < 0.0) {
 			cosThetai = -cosThetai;
-			n = n.inverse();
+			n.inverse();
 			eta = 1.0/eta;
 		}
 		double etaSquared = eta*eta;
 		double temp = 1.0 - (1.0 - cosThetai * cosThetai)/etaSquared;
 		double cosTheta2 = Math.sqrt(temp);
-		wt.copy(wo.inverse().scale(1.0 / eta)
-				.sub(n.scale(cosTheta2 - cosThetai / eta)));
+		
+		mWo.inverse();
+		mWo.scale(1.0 / eta);
+		n.scale(cosTheta2 - cosThetai / eta);
+		mWo.sub(n);
+		wt.copy(mWo);
+		
 		final double aux = Math.abs(sr.normal.dot(wt));
-		return (Color.whiteColor().multiply(kt/etaSquared))
-				.multiply(1.0/aux);
+		return (Color.whiteColor().multiplyEquals(kt/etaSquared))
+				.multiplyEquals(1.0/aux);
 	}
 
 	@Override
