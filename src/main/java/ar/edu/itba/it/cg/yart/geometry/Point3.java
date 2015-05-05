f79cc8a751a8c297ab32567ac909abacec682062
Facundo Menzella <fmenzella@monits.com>
2015-05-05 15:04:17 -0300

    transformByMatrix added to point3

--- src/main/java/ar/edu/itba/it/cg/yart/geometry/Point3.java
+++ src/main/java/ar/edu/itba/it/cg/yart/geometry/Point3.java
@@ -1,10 +1,12 @@
 package ar.edu.itba.it.cg.yart.geometry;
 
+import ar.edu.itba.it.cg.yart.transforms.Matrix4d;
+
 public class Point3 {
 	
-	public double x;
-	public double y;
-	public double z;
+	public final double x;
+	public final double y;
+	public final double z;
 	
 	public Point3(final double x, final double y, final double z) {
 		this.x = x;
@@ -42,4 +44,11 @@ public class Point3 {
 		this.z = p.z;
 	}
 
+	public Point3 transformByMatrix(final Matrix4d matrix) {
+		final double dx = (matrix.m00 * this.x) + (matrix.m01 * this.y) + (matrix.m02 * this.z) + matrix.m03;
+		final double dy = (matrix.m10 * this.x) + (matrix.m11 * this.y) + (matrix.m12 * this.z) + matrix.m13;
+		final double dz = (matrix.m20 * this.x) + (matrix.m21 * this.y) + (matrix.m22 * this.z) + matrix.m23;
+		return new Point3(dx, dy, dz);
+	}
+	
 }
