bd5a9d0667e2c8318195e9ddffa8ee165ef9704e
Facundo Menzella <fmenzella@monits.com>
2015-05-05 15:11:48 -0300

    lights implement transformable

--- src/main/java/ar/edu/itba/it/cg/yart/light/AbstractLight.java
+++ src/main/java/ar/edu/itba/it/cg/yart/light/AbstractLight.java
@@ -4,8 +4,9 @@ import ar.edu.itba.it.cg.yart.color.Color;
 import ar.edu.itba.it.cg.yart.geometry.Vector3d;
 import ar.edu.itba.it.cg.yart.raytracer.Ray;
 import ar.edu.itba.it.cg.yart.raytracer.ShadeRec;
+import ar.edu.itba.it.cg.yart.transforms.Transformable;
 
-public abstract class AbstractLight implements Light {
+public abstract class AbstractLight implements Light, Transformable {
 
 	protected boolean shadows;
 
--- src/main/java/ar/edu/itba/it/cg/yart/light/AmbientLight.java
+++ src/main/java/ar/edu/itba/it/cg/yart/light/AmbientLight.java
@@ -5,6 +5,7 @@ import ar.edu.itba.it.cg.yart.color.Color;
 import ar.edu.itba.it.cg.yart.geometry.Vector3d;
 import ar.edu.itba.it.cg.yart.raytracer.Ray;
 import ar.edu.itba.it.cg.yart.raytracer.ShadeRec;
+import ar.edu.itba.it.cg.yart.transforms.Matrix4d;
 
 public class AmbientLight extends AbstractLight {
 	
@@ -38,6 +39,11 @@ public class AmbientLight extends AbstractLight {
 	@Override
 	public boolean inShadow(Ray ray, ShadeRec sr) {
 		return false;
+	}
+
+	@Override
+	public void applyTransformation(Matrix4d matrix) {
+		this.direction = this.direction.transformByMatrix(matrix);
 	}
 
 }
