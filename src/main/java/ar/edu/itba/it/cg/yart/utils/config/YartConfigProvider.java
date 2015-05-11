a638e7a83c30ded2e5faccbffeb79321db151747
Gustavo Del Giudice <gdelgiud@alu.itba.edu.ar>
2015-05-10 23:27:59 -0300

    Added more command line parameters.

--- src/main/java/ar/edu/itba/it/cg/yart/utils/config/YartConfigProvider.java
+++ src/main/java/ar/edu/itba/it/cg/yart/utils/config/YartConfigProvider.java
@@ -3,6 +3,9 @@ package ar.edu.itba.it.cg.yart.utils.config;
 public class YartConfigProvider {
 
 	private static final String CORES_QTY_KEY = "cores";
+	private static final String BUCKET_SIZE_KEY = "bucketSize";
+	private static final String MAX_T_KEY = "maxT";
+	private static final String DISTANCE_KEY = "distance";
 	
 	private final YartConfigReader reader;
 	
@@ -13,6 +16,21 @@ public class YartConfigProvider {
 	public int getCoresQty() {
 		final String qty = this.reader.getKey(CORES_QTY_KEY);
 		return Integer.valueOf(qty);
+	}
+	
+	public int getBucketSize() {
+		final String size = this.reader.getKey(BUCKET_SIZE_KEY);
+		return Integer.valueOf(size);
+	}
+	
+	public int getMaxT() {
+		final String maxT = this.reader.getKey(MAX_T_KEY);
+		return Integer.valueOf(maxT);
+	}
+	
+	public int getDistance() {
+		final String distance = this.reader.getKey(DISTANCE_KEY);
+		return Integer.valueOf(distance);
 	}
 	
 }
