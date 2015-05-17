03135612604213d832e63babf937c351ca829c77
Facundo Menzella <fmenzella@monits.com>
2015-05-17 17:33:40 -0300

    new tracer on da house HitTracer. Now we have HitTracer, ShadowTracer and ColorTracer

---
+++ src/main/java/ar/edu/itba/it/cg/yart/raytracer/tracer/SimpleShadowTracer.java
@@ -0,0 +1,31 @@
+package ar.edu.itba.it.cg.yart.raytracer.tracer;
+
+import java.util.List;
+
+import ar.edu.itba.it.cg.yart.geometry.primitives.GeometricObject;
+import ar.edu.itba.it.cg.yart.raytracer.Ray;
+
+public class SimpleShadowTracer extends AbstractTracer implements ShadowTracer{
+
+	@Override
+	public double traceShadowHit(final Ray ray, final List<GeometricObject> objects, final double tMax) {
+		if (ray.depth >= MAX_DEPTH) {
+			return Double.NEGATIVE_INFINITY;
+		}
+		double tMin = tMax;
+		boolean hit = false;
+		for (int i = 0; i < objects.size(); i++) {
+			GeometricObject object = objects.get(i);
+			double t = object.shadowHit(ray);
+			if (t != Double.NEGATIVE_INFINITY && t < tMin) {
+				tMin = t;
+				hit = true;
+			}
+		}
+		if (hit) {
+			return tMin;
+		}
+		return Double.NEGATIVE_INFINITY;
+	}
+
+}
