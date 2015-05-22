449e41aeb29a818cb401a3390a1d4bce211de208
Facundo Menzella <fmenzella@monits.com>
2015-05-21 23:31:17 -0300

    removed hitTracer

--- src/main/java/ar/edu/itba/it/cg/yart/acceleration_estructures/fkdtree/nlog2n/YAFKDTree2.java
+++ src/main/java/ar/edu/itba/it/cg/yart/acceleration_estructures/fkdtree/nlog2n/YAFKDTree2.java
@@ -14,6 +14,7 @@ import ar.edu.itba.it.cg.yart.acceleration_estructures.fkdtree.SplitAxis;
 import ar.edu.itba.it.cg.yart.acceleration_estructures.fkdtree.SplitPoint;
 import ar.edu.itba.it.cg.yart.color.Color;
 import ar.edu.itba.it.cg.yart.geometry.Point3d;
+import ar.edu.itba.it.cg.yart.geometry.Vector3d;
 import ar.edu.itba.it.cg.yart.geometry.primitives.AABB;
 import ar.edu.itba.it.cg.yart.geometry.primitives.GeometricObject;
 import ar.edu.itba.it.cg.yart.raytracer.Ray;
@@ -523,17 +524,41 @@ public class YAFKDTree2 {
 		}
 	}
 
-	public double traceRayHit(final Ray ray, final HitTracer tracer,
-			final ShadeRec sr) {
-		return p_traceObjectsForRayHit(ray, root, kEPSILON, kTMAX, tracer, sr);
+	public double traceRayHit(final Ray ray, final ShadeRec sr) {
+		return p_traceObjectsForRayHit(ray, root, kEPSILON, kTMAX, sr);
 	}
 
 	private double p_traceObjectsForRayHit(final Ray ray, final KDNode node,
-			final double min, final double max, final HitTracer tracer,
-			final ShadeRec sr) {
+			final double min, final double max, final ShadeRec sr) {
 
 		if (node.isLeaf()) {
-			return tracer.traceRayHit(ray, node.gObjects, sr, max);
+			KDLeafNode leaf = (KDLeafNode)node;
+			List<GeometricObject> objects = leaf.gObjects;
+			if (objects == null || objects.size() == 0) {
+				return Double.NEGATIVE_INFINITY;
+			}
+			
+			double tMin = max;
+			boolean hit = false;
+			Vector3d normal = null;
+			Point3d localHitPoint = null;
+			GeometricObject object = null;
+			for (int i = 0; i < objects.size(); i++) {
+				object = objects.get(i);
+				double t = object.hit(ray, sr);
+				if (t != Double.NEGATIVE_INFINITY && t < tMin) {
+					tMin = t;
+					normal = sr.normal;
+					localHitPoint = sr.localHitPoint;
+					hit = true;
+				}
+			}
+			if (hit) {
+				sr.normal = normal;
+				sr.localHitPoint = localHitPoint;
+				return tMin;
+			}
+			return Double.NEGATIVE_INFINITY;
 		}
 
 		double dir[] = { ray.direction.x, ray.direction.y, ray.direction.z };
@@ -561,19 +586,18 @@ public class YAFKDTree2 {
 		// This is madness !!
 		if (t > max || t < kEPSILON) {
 			// its on the near node
-			return p_traceObjectsForRayHit(ray, near, min, max, tracer, sr);
+			return p_traceObjectsForRayHit(ray, near, min, max, sr);
 		} else {
 			if (t < min) {
 				// its on the far node
-				return p_traceObjectsForRayHit(ray, far, min, max, tracer, sr);
+				return p_traceObjectsForRayHit(ray, far, min, max, sr);
 			} else {
 				// the ray might hit in both nodes, so we split the ray
-				double distance = p_traceObjectsForRayHit(ray, near, min, t,
-						tracer, sr);
+				double distance = p_traceObjectsForRayHit(ray, near, min, t, sr);
 				if (distance != Double.NEGATIVE_INFINITY) {
 					return distance;
 				}
-				return p_traceObjectsForRayHit(ray, far, t, max, tracer, sr);
+				return p_traceObjectsForRayHit(ray, far, t, max, sr);
 			}
 		}
 	}
