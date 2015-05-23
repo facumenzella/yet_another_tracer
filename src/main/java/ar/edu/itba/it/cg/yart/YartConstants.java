7bef2b492a33ce1169c450fd07197fa204a4c374
Gustavo Del Giudice <gdelgiud@alu.itba.edu.ar>
2015-05-23 15:36:54 -0300

    More parser logging work.

--- src/main/java/ar/edu/itba/it/cg/yart/YartConstants.java
+++ src/main/java/ar/edu/itba/it/cg/yart/YartConstants.java
@@ -3,5 +3,8 @@ package ar.edu.itba.it.cg.yart;
 public class YartConstants {
 	
 	public static final String LOG_FILE = "yart.log";
+	
+	public static final int DEFAULT_XRES = 800;
+	public static final int DEFAULT_YRES = 600;
 
 }
