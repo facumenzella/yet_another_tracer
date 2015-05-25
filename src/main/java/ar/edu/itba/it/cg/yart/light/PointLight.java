91006263c6939b3c4cac711a4d3fe1f8d0c3860b
Facundo Menzella <fmenzella@monits.com>
2015-05-25 13:42:14 -0300

    more efficient glossy specular

--- src/main/java/ar/edu/itba/it/cg/yart/light/PointLight.java
+++ src/main/java/ar/edu/itba/it/cg/yart/light/PointLight.java
@@ -1,70 +1,70 @@
-package ar.edu.itba.it.cg.yart.light;
-
-import ar.edu.itba.it.cg.yart.acceleration_estructures.fkdtree.Stack;
-import ar.edu.itba.it.cg.yart.color.Color;
-import ar.edu.itba.it.cg.yart.geometry.Point3d;
-import ar.edu.itba.it.cg.yart.geometry.Vector3d;
-import ar.edu.itba.it.cg.yart.raytracer.Ray;
-import ar.edu.itba.it.cg.yart.raytracer.ShadeRec;
-import ar.edu.itba.it.cg.yart.transforms.Matrix4d;
-
-public class PointLight extends AbstractLight {
-
-	private final double ls;
-	private final Color color;
-	private Vector3d location;
-	private Point3d point;
-
-	public PointLight(final double ls, final Color color,
-			final Vector3d location) {
-		super();
-		this.ls = ls;
-		this.color = color;
-		this.location = location;
-		this.point = new Point3d(location.x, location.y, location.z);
-	}
-
-	public PointLight(final double ls, final Color color) {
-		this(ls, color, new Vector3d(0, 0, 0));
-	}
-
-	@Override
-	public Vector3d getDirection(final ShadeRec sr) {
-		final double dx = location.x - sr.hitPoint.x;
-		final double dy = location.y - sr.hitPoint.y;
-		final double dz = location.z - sr.hitPoint.z;
-		final double length = Math.sqrt(dx * dx + dy * dy + dz * dz);
-		return new Vector3d(dx / length, dy / length, dz / length);
-	}
-
-	@Override
-	public Color L(final ShadeRec sr) {
-		final double r = color.r * ls;
-		final double g = color.g * ls;
-		final double b = color.b * ls;
-		return new Color(r, g, b, color.a);
-	}
-
-	@Override
-	public boolean inShadow(final Ray ray, final ShadeRec sr, final Stack stack) {
-		double t;
-
-		final double dx = point.x - ray.origin.x;
-		final double dy = point.y - ray.origin.y;
-		final double dz = point.z - ray.origin.z;
-
-		final double d = Math.sqrt(dx * dx + dy * dy + dz * dz);
-
-		t = sr.world.getTree().traceShadowHit(ray, stack);
-		if (t != Double.NEGATIVE_INFINITY && t < d) {
-			return true;
+	package ar.edu.itba.it.cg.yart.light;
+	
+	import ar.edu.itba.it.cg.yart.acceleration_estructures.fkdtree.Stack;
+	import ar.edu.itba.it.cg.yart.color.Color;
+	import ar.edu.itba.it.cg.yart.geometry.Point3d;
+	import ar.edu.itba.it.cg.yart.geometry.Vector3d;
+	import ar.edu.itba.it.cg.yart.raytracer.Ray;
+	import ar.edu.itba.it.cg.yart.raytracer.ShadeRec;
+	import ar.edu.itba.it.cg.yart.transforms.Matrix4d;
+	
+	public class PointLight extends AbstractLight {
+	
+		private final double ls;
+		private final Color color;
+		private Vector3d location;
+		private Point3d point;
+	
+		public PointLight(final double ls, final Color color,
+				final Vector3d location) {
+			super();
+			this.ls = ls;
+			this.color = color;
+			this.location = location;
+			this.point = new Point3d(location.x, location.y, location.z);
 		}
-		return false;
+	
+		public PointLight(final double ls, final Color color) {
+			this(ls, color, new Vector3d(0, 0, 0));
+		}
+	
+		@Override
+		public Vector3d getDirection(final ShadeRec sr) {
+			final double dx = location.x - sr.hitPoint.x;
+			final double dy = location.y - sr.hitPoint.y;
+			final double dz = location.z - sr.hitPoint.z;
+			final double length = Math.sqrt(dx * dx + dy * dy + dz * dz);
+			return new Vector3d(dx / length, dy / length, dz / length);
+		}
+	
+		@Override
+		public Color L(final ShadeRec sr) {
+			final double r = color.r * ls;
+			final double g = color.g * ls;
+			final double b = color.b * ls;
+			return new Color(r, g, b, color.a);
+		}
+	
+		@Override
+		public boolean inShadow(final Ray ray, final ShadeRec sr, final Stack stack) {
+			double t;
+	
+			final double dx = point.x - ray.origin.x;
+			final double dy = point.y - ray.origin.y;
+			final double dz = point.z - ray.origin.z;
+	
+			final double d = Math.sqrt(dx * dx + dy * dy + dz * dz);
+	
+			t = sr.world.getTree().traceShadowHit(ray, stack);
+			if (t != Double.NEGATIVE_INFINITY && t < d) {
+				return true;
+			}
+			return false;
+		}
+	
+		@Override
+		public void applyTransformation(Matrix4d matrix) {
+			this.point = this.point.transformByMatrix(matrix);
+			location = new Vector3d(point.x, point.y, point.z);
+		}
 	}
-
-	@Override
-	public void applyTransformation(Matrix4d matrix) {
-		this.point = this.point.transformByMatrix(matrix);
-		location = new Vector3d(point.x, point.y, point.z);
-	}
-}
--- src/main/java/ar/edu/itba/it/cg/yart/light/brdf/GlossySpecular.java
+++ src/main/java/ar/edu/itba/it/cg/yart/light/brdf/GlossySpecular.java
@@ -1,7 +1,6 @@
 package ar.edu.itba.it.cg.yart.light.brdf;
 
 import ar.edu.itba.it.cg.yart.color.Color;
-import ar.edu.itba.it.cg.yart.geometry.MutableVector3d;
 import ar.edu.itba.it.cg.yart.geometry.Vector3d;
 import ar.edu.itba.it.cg.yart.raytracer.ShadeRec;
 
@@ -11,21 +10,33 @@ public class GlossySpecular extends BRDF{
 	private double exp;
 	
 	@Override
-	public Color f(final ShadeRec sr, final Vector3d wo, final Vector3d wi) {
-		MutableVector3d mNormalSR = new MutableVector3d(sr.normal);
-		MutableVector3d mWi = new MutableVector3d(wi);
-		mWi.inverse();
+	public Color f(final ShadeRec sr, final Vector3d wo, final Vector3d wi) {		
+		double wix = -wi.x;
+		double wiy = -wi.y;
+		double wiz = -wi.z;
 		
 		Color L = null;
 		final double NdotWi = sr.normal.dot(wi);
-		mNormalSR.scale(2.0);
-		mNormalSR.scale(NdotWi);
-		mWi.add(mNormalSR);
-		final double rdotWo = mWi.dot(wo);
+		
+		final double nx = sr.normal.x * 2 * NdotWi;
+		final double ny = sr.normal.y * 2 * NdotWi;
+		final double nz = sr.normal.z * 2 * NdotWi;
+		
+		wix += nx;
+		wiy += ny;
+		wiz += nz;
+
+		final double rdoWox = wix * wo.x;
+		final double rdoWoy = wiy * wo.y;
+		final double rdoWoz = wiz * wo.z;
+
+		final double rdotWo = rdoWox + rdoWoy + rdoWoz;
 		
 		if(rdotWo >	0.0) {
 			final double aux = ks* Math.pow(rdotWo, exp);
 			L = new Color(aux,aux,aux);
+		} else {
+			return new Color(0, 0, 0);
 		}
 		return L;
 	}
