a1d801fbfaa73baefbe22272477585138104ad1b
Facundo Menzella <fmenzella@monits.com>
2015-06-10 20:01:46 -0300

    equals and hashcode added to split point

--- src/main/java/ar/edu/itba/it/cg/yart/acceleration_estructures/fkdtree/SplitPoint.java
+++ src/main/java/ar/edu/itba/it/cg/yart/acceleration_estructures/fkdtree/SplitPoint.java
@@ -1,5 +1,9 @@
 package ar.edu.itba.it.cg.yart.acceleration_estructures.fkdtree;
 
+import java.util.Objects;
+
+import ar.edu.itba.it.cg.yart.acceleration_estructures.fkdtree.nlog2n.YAFKDTree;
+
 public class SplitPoint {
 	
 	public int axis; // x = 0, y = 1, z = 2
@@ -16,5 +20,24 @@ public class SplitPoint {
 	@Override
 	public String toString() {
 		return "Axis " + axis + ": "+ point;
+	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		if (obj == null) {
+			return false;
+		}
+		if (!(obj instanceof SplitPoint)) {
+			return false;
+		}
+		final SplitPoint o = (SplitPoint) obj;
+
+		return axis == o.axis && point > o.point - YAFKDTree.kEPSILON
+				&& point < o.point + YAFKDTree.kEPSILON;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(axis, point);
 	}
 }
