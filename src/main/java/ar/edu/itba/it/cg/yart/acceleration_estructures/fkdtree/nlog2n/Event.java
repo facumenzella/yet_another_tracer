8f359935b8e2b322716420f72c05df76afc7bc46
Facundo Menzella <fmenzella@monits.com>
2015-06-25 09:31:47 -0300

    moved mergeEvents method to Event class

--- src/main/java/ar/edu/itba/it/cg/yart/acceleration_estructures/fkdtree/nlog2n/Event.java
+++ src/main/java/ar/edu/itba/it/cg/yart/acceleration_estructures/fkdtree/nlog2n/Event.java
@@ -1,5 +1,10 @@
 package ar.edu.itba.it.cg.yart.acceleration_estructures.fkdtree.nlog2n;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+
 import ar.edu.itba.it.cg.yart.acceleration_estructures.fkdtree.SplitPoint;
 import ar.edu.itba.it.cg.yart.geometry.primitives.GeometricObject;
 
@@ -38,5 +43,55 @@ public class Event implements Comparable<Event> {
 	public String toString() {
 		return "Event [axis=" + axis + ", type=" + type + ", position="
 				+ point + object + "]";
+	}
+	
+	public static List<Event> mergeEvents(final Collection<Event> e1,
+			final Collection<Event> elo) {
+		final Iterator<Event> it1 = e1.iterator();
+		final Iterator<Event> it2 = elo.iterator();
+		final List<Event> merged = new ArrayList<>();
+
+		if (!it1.hasNext()) {
+			merged.addAll(elo);
+			return merged;
+		}
+
+		if (!it2.hasNext()) {
+			merged.addAll(e1);
+			return merged;
+		}
+
+		Event ev1 = it1.next();
+		Event ev2 = it2.next();
+
+		do {
+			if (ev1.compareTo(ev2) < 0) {
+				merged.add(ev1);
+				if (it1.hasNext()) {
+					ev1 = it1.next();
+				} else {
+					merged.add(ev2);
+					break;
+				}
+			} else {
+				merged.add(ev2);
+				if (it2.hasNext()) {
+					ev2 = it2.next();
+				} else {
+					merged.add(ev1);
+					break;
+				}
+			}
+		} while (it1.hasNext() && it2.hasNext());
+
+		while (it1.hasNext()) {
+			merged.add(it1.next());
+		}
+
+		while (it2.hasNext()) {
+			merged.add(it2.next());
+		}
+
+		return merged;
 	}
 }
