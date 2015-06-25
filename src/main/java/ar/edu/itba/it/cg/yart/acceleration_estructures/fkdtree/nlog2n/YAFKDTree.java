2c13d9ecd51e8d8053b8085d599e94cd411c5f29
Facundo Menzella <fmenzella@monits.com>
2015-06-25 09:43:18 -0300

    Classifications moved to ClassifiedEvents and ClassifiedObjects

--- src/main/java/ar/edu/itba/it/cg/yart/acceleration_estructures/fkdtree/nlog2n/YAFKDTree.java
+++ src/main/java/ar/edu/itba/it/cg/yart/acceleration_estructures/fkdtree/nlog2n/YAFKDTree.java
@@ -136,10 +136,10 @@ public class YAFKDTree {
 		AABB[] boxes = bestCandidate.boxes;
 
 		// We classify the objects with the events (left, right, both)
-		ClassifiedObjects classifiedObjects = classify(gObjects, events,
+		ClassifiedObjects classifiedObjects = ClassifiedObjects.classify(gObjects, events,
 				bestCandidate);
 		// We classify the events in left and right
-		final ClassifiedEvents classifiedEvents = splice(events,
+		final ClassifiedEvents classifiedEvents = ClassifiedEvents.splice(events,
 				classifiedObjects, boxes[0], boxes[1]);
 
 		final Event[] ebl = classifiedEvents.ebl.toArray(new Event[0]);
@@ -726,88 +726,4 @@ public class YAFKDTree {
 		}
 	}
 
-	public static ClassifiedEvents splice(final Event[] events,
-			final ClassifiedObjects tc, final AABB leftBox, final AABB rightBox) {
-		final List<Event> elo = new ArrayList<Event>(); // left only
-		final List<Event> ero = new ArrayList<Event>(); // right only
-		final List<Event> ebl = new ArrayList<Event>(); // events overlapping
-														// left
-		final List<Event> ebr = new ArrayList<Event>(); // events overlapping
-														// right
-
-		for (int i = 0; i < events.length; i++) {
-			Event e = events[i];
-			switch (tc.sides.get(e.object)) {
-			// events for “both sides”(3) triangles get discarded
-			case 1:
-				elo.add(e);
-				break;
-			case 2:
-				ero.add(e);
-				break;
-			}
-		}
-
-		for (final Entry<GeometricObject, Integer> entry : tc.sides.entrySet()) {
-			if (entry.getValue() == 3) {
-				final GeometricObject obj = entry.getKey();
-				ebl.addAll(YAFKDTree.generateEvents(obj, leftBox));
-				ebr.addAll(YAFKDTree.generateEvents(obj, rightBox));
-			}
-		}
-
-		return new ClassifiedEvents(elo, ero, ebl, ebr);
-	}
-	
-	public static ClassifiedObjects classify(List<GeometricObject> gObjects,
-			final Event[] events, final PlaneCandidate candidate) {
-		// 1: Left
-		// 2: Right
-		// 3: Both
-		Map<GeometricObject, Integer> sides = new HashMap<GeometricObject, Integer>();
-
-		for (final GeometricObject o : gObjects) {
-			sides.put(o, 3);
-		}
-
-		// END(0), PLANAR(1), START(2);
-		for (final Event e : events) {
-			if (e.type == 0 && e.axis == candidate.splitPoint.axis
-					&& e.point <= candidate.splitPoint.point) {
-				sides.put(e.object, 1);
-			} else if (e.type == 2 && e.axis == candidate.splitPoint.axis
-					&& e.point >= candidate.splitPoint.point) {
-				sides.put(e.object, 2);
-			} else if (e.type == 1 && e.axis == candidate.splitPoint.axis) {
-				if (e.point < candidate.splitPoint.point
-						|| (e.point == candidate.splitPoint.point && candidate.left)) {
-					sides.put(e.object, 1);
-				} else if (e.point > candidate.splitPoint.point
-						|| (e.point == candidate.splitPoint.point && !candidate.left)) {
-					sides.put(e.object, 2);
-				}
-
-			}
-		}
-
-		final List<GeometricObject> tl = new ArrayList<GeometricObject>();
-		final List<GeometricObject> tr = new ArrayList<GeometricObject>();
-
-		for (final Entry<GeometricObject, Integer> e : sides.entrySet()) {
-			switch (e.getValue()) {
-			case 1:
-				tl.add(e.getKey());
-				break;
-			case 2:
-				tr.add(e.getKey());
-				break;
-			case 3:
-				tl.add(e.getKey());
-				tr.add(e.getKey());
-				break;
-			}
-		}
-
-		return new ClassifiedObjects(tl, tr, sides);
-	}
 }
