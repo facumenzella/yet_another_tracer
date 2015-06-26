729e34af43399be15cffd8493dcd2737679c99df
Gustavo Del Giudice <gdelgiud@alu.itba.edu.ar>
2015-06-26 18:58:11 -0300

    More Area Light work.

--- src/main/java/ar/edu/itba/it/cg/yart/light/materials/Emissive.java
+++ src/main/java/ar/edu/itba/it/cg/yart/light/materials/Emissive.java
@@ -2,14 +2,31 @@ package ar.edu.itba.it.cg.yart.light.materials;
 
 import ar.edu.itba.it.cg.yart.acceleration_estructures.fkdtree.Stack;
 import ar.edu.itba.it.cg.yart.color.Color;
+import ar.edu.itba.it.cg.yart.geometry.Vector3d;
 import ar.edu.itba.it.cg.yart.raytracer.ShadeRec;
 
 public class Emissive extends MaterialAbstract {
+	
+	private double ls = 1;
+	private Color ce = Color.WHITE;
+	private Color realCe = ce;
+	
+	public void setCe(final Color ce) {
+		this.realCe = ce.multiply(ls);
+	}
+	
+	public void setLs(final double ls) {
+		this.ls = ls;
+		setCe(ce);
+	}
 
 	@Override
 	public Color shade(ShadeRec sr, Stack stack) {
-		// TODO Auto-generated method stub
-		return null;
+		if (sr.normal.inverse().dot(new Vector3d(sr.ray.direction[0], sr.ray.direction[1], sr.ray.direction[2])) > 0) {
+			return realCe;
+		}
+		
+		return Color.BLACK;
 	}
 
 }
