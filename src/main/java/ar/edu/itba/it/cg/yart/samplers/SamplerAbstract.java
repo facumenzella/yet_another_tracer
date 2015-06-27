26994687d993eac67e16583fd7bcb8e8e2a38315
Facundo Menzella <fmenzella@monits.com>
2015-06-27 17:06:34 -0300

    sampler now maps samples to spheres and hemispheres

--- src/main/java/ar/edu/itba/it/cg/yart/samplers/SamplerAbstract.java
+++ src/main/java/ar/edu/itba/it/cg/yart/samplers/SamplerAbstract.java
@@ -4,6 +4,7 @@ import java.util.Arrays;
 import java.util.Random;
 
 import ar.edu.itba.it.cg.yart.geometry.Point2d;
+import ar.edu.itba.it.cg.yart.geometry.Point3d;
 
 /*
  * For more information check out chapter 5 in RTFTGU
@@ -15,6 +16,8 @@ public abstract class SamplerAbstract implements Sampler{
 	final protected int num_samples; // the number of points in a pattern
 	final protected int num_sets; // the number of sample sets (patterns) stored
 	final protected Point2d[] samples;  // sample points  on a unit square
+	final protected Point3d[] hemisphereSamples; // sample points on a unit hemisphere
+	final protected Point3d[] sphereSamples; // sample points on a unit sphere
 	final protected int[] shuffled_indices; // shuffled samples array indices
 	protected long count; // the current number of sample points used
 	protected int jump; // random index jump
@@ -23,6 +26,8 @@ public abstract class SamplerAbstract implements Sampler{
 		this.num_samples = num_samples;
 		this.num_sets = num_sets;
 		this.samples = new Point2d[num_samples*num_sets];
+		this.hemisphereSamples = new Point3d[num_samples * num_sets];
+		this.sphereSamples = new Point3d[num_samples * num_sets];
 		this.shuffled_indices = setupShuffledIndices();
 		this.random = new Random();
 	}
@@ -48,7 +53,7 @@ public abstract class SamplerAbstract implements Sampler{
 		return shuffled_indices;
 	}
 	
-	protected void shuffle_x_coordinates() {
+	protected void shuffleXCoordinates() {
 		for (int p = 0; p < num_sets; p++)
 			for (int i = 0; i <  num_samples - 1; i++) {
 				int target = random.nextInt() % num_samples + p * num_samples;
@@ -58,7 +63,7 @@ public abstract class SamplerAbstract implements Sampler{
 			}
 	}
 	
-	protected void shuffle_y_coordinates() {
+	protected void shuffleYCoordinates() {
 		for (int p = 0; p < num_sets; p++)
 			for (int i = 0; i <  num_samples - 1; i++) {
 				int target = random.nextInt() % num_samples + p * num_samples;
@@ -74,5 +79,37 @@ public abstract class SamplerAbstract implements Sampler{
 			jump = (random.nextInt() % num_sets) * num_samples;
 		}
 		return this.samples[((int) (jump + count++ % num_samples))];
+	}
+	
+	public void mapSamples2Hemisphere(final double exp) {
+		int size = samples.length;
+		
+		for (int j = 0; j < size; j++) {
+			final double cos_phi = Math.cos(2.0 * Math.PI * samples[j].x);
+			final double sin_phi = Math.sin(2.0 * Math.PI * samples[j].x);	
+			final double cos_theta = Math.pow((1.0 - samples[j].y), 1.0 / (exp + 1.0));
+			final double sin_theta = Math.sqrt(1.0 - cos_theta * cos_theta);
+			final double pu = sin_theta * cos_phi;
+			final double pv = sin_theta * sin_phi;
+			final double pw = cos_theta;
+			this.hemisphereSamples[j] = new Point3d(pu, pv, pw); 
+		}
+	}
+	
+	public void mapSamples2Sphere() {
+		double r1, r2;
+		double x, y, z;
+		double r, phi;
+			
+		for (int j = 0; j < num_samples * num_sets; j++) {
+			r1 	= samples[j].x;
+	    	r2 	= samples[j].y;
+	    	z 	= 1.0 - 2.0 * r1;
+	    	r 	= Math.sqrt(1.0 - z * z);
+	    	phi = Math.PI * 2 * r2;
+	    	x 	= r * Math.cos(phi);
+	    	y 	= r * Math.sin(phi);
+			this.sphereSamples[j] = new Point3d(x, y, z); 
+		}
 	}
 }
