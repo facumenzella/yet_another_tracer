e3ee11508681fffd88323383afd8bb209d661cb5
Facundo Menzella <fmenzella@monits.com>
2015-06-29 16:15:47 -0300

    global shade for emmisisve

--- src/main/java/ar/edu/itba/it/cg/yart/light/materials/Emissive.java
+++ src/main/java/ar/edu/itba/it/cg/yart/light/materials/Emissive.java
@@ -46,4 +46,19 @@ public class Emissive extends MaterialAbstract {
 		return backfaceMaterial.shade(sr, stack);
 	}
 
+	@Override
+	public Color globalShade(ShadeRec sr, Stack stack) {
+		if (sr.ray.depth == 1) {
+			return Color.BLACK;
+		}
+		Vector3d invSr = sr.normal.inverse();
+		
+		final double dot = invSr.x * sr.ray.direction[0] + invSr.y * sr.ray.direction[1] + invSr.z * sr.ray.direction[2];
+		
+		if (dot > 0.0) {
+			return ce;
+		}
+		return Color.BLACK;
+	}
+
 }
