e56d7f283055657f96d8dcfe0c2ba42de7f7f141
Facundo Menzella <fmenzella@monits.com>
2015-06-29 15:41:55 -0300

    map sampler to hemisphere call added to glossy specular

--- src/main/java/ar/edu/itba/it/cg/yart/samplers/Sampler.java
+++ src/main/java/ar/edu/itba/it/cg/yart/samplers/Sampler.java
@@ -6,7 +6,8 @@ import ar.edu.itba.it.cg.yart.geometry.Point3d;
 public interface Sampler {
 
 	public Point2d sampleUnitSquare(); // get the next sample on unit square
-	public Point3d sample_hemisphere();
-	public Point3d sample_sphere();
-	
+	public Point3d sampleHemisphere();
+	public Point3d sampleSphere();
+	public void mapSamples2Hemisphere(final double exp);
+
 }
