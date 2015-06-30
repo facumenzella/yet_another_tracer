3f6cdd55706c8d902758a0d6eddfff463484fd01
Gustavo Del Giudice <gdelgiud@alu.itba.edu.ar>
2015-06-29 23:47:12 -0300

    Implemented light power and efficacy

--- src/main/java/ar/edu/itba/it/cg/yart/light/materials/Emissive.java
+++ src/main/java/ar/edu/itba/it/cg/yart/light/materials/Emissive.java
@@ -54,7 +54,7 @@ public class Emissive extends MaterialAbstract {
 		Vector3d invSr = sr.normal.inverse();
 		final double dot = invSr.x * sr.ray.direction[0] + invSr.y * sr.ray.direction[1] + invSr.z * sr.ray.direction[2];
 		if (dot > 0.0) {
-			return ce;
+			return realCe;
 		}
 		
 		return backfaceMaterial.shade(sr, stack);
