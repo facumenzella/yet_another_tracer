7a7c2b24bc08eedce6cd0fc7da4c01bc532a02c2
Facundo Menzella <fmenzella@monits.com>
2015-07-02 15:23:39 -0300

    we do not compute indirect light twice

--- src/main/java/ar/edu/itba/it/cg/yart/light/materials/Emissive.java
+++ src/main/java/ar/edu/itba/it/cg/yart/light/materials/Emissive.java
@@ -39,6 +39,9 @@ public class Emissive extends MaterialAbstract {
 
 	@Override
 	public Color shade(ShadeRec sr, Stack stack) {
+		if (sr.ray.depth == 1) {
+			return Color.BLACK;
+		}
 		if (sr.normal.inverse().dot(new Vector3d(sr.ray.direction[0], sr.ray.direction[1], sr.ray.direction[2])) > 0) {
 			if (sr.ray.depth != 0) {
 				return realCe.multiply(Math.pow(1 / sr.t, 2));
